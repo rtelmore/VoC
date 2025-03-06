a7412c5cfda3ff3568fe28c3f6a4e95947c97ff7
Ryan Elmore <rtelmore@gmail.com>
2025-03-06 08:30:27 -0700

    figs for paper vertical line

--- r/04-figs-for-paper.R
+++ r/04-figs-for-paper.R
@@ -35,25 +35,49 @@ p + geom_boxplot() +
 
 ggsave("fig/boxplots-ts-12.png", height = 8.5, width = 8.5)
 
-p <- ggplot(data = tb |> 
-              filter(date >= lubridate::ymd("1950-01-01"),
-                     window == 120),
+aaa_baa <- read.csv("data/aaa-baa.csv") |> 
+  select(-Y) |> 
+  rename(AAA = exAAARF, BAA = exBAARF) |> 
+  pivot_longer(-1, names_to = "penalty_f", values_to = "ts") |> 
+  mutate(date = ymd(date),
+         method = "Ridge/GW") |> 
+  rbind(read.csv("data/aaa-baa.csv") |> 
+          select(-Y) |> 
+          rename(AAA = exAAARF, BAA = exBAARF) |> 
+          pivot_longer(-1, names_to = "penalty_f", values_to = "ts") |> 
+          mutate(date = ymd(date),
+                 method = "Ridge/RFF"))
+
+aaa_baa$penalty_f = factor(aaa_baa$penalty_f, 
+                      levels = c("10e-3", "10e-2", "10e-1", "10",
+                                 "10e+1", "10e+2", "10e+3", "AAA", "BAA"))
+
+tmp <- tb |> 
+  filter(penalty %in% c("10e-3", "10e-1", "10e+1", "10e+3"),
+         window == 120) |> 
+  bind_rows(aaa_baa) |> 
+  filter(date >= ymd("1950-01-01")) |> 
+  mutate(bond = ifelse(penalty_f %in% c("AAA", "BAA"),
+                       "Bond", "Ridge"))
+
+p <- ggplot(data = tmp,
             aes(x = penalty_f, fill = penalty_f, y = ts))
 p + geom_boxplot() + 
   facet_grid(method ~ .) +
   scale_fill_brewer("Ridge Penalty", palette = "Dark2") +
-  labs(x = "Ridge Penalty",
-       y = "Estimated Timing Strategy") +
-  scale_y_continuous(limits = c(-0.001, 0.001)) +
+  labs(x = "Ridge Penalty and Bond Type",
+       y = "Estimated Timing Strategy and Bond Returns") +
+  scale_y_continuous(limits = c(-0.01, 0.01)) +
   theme_bw() +
+  geom_vline(xintercept = 4.5, linetype = "dashed") +
   guides(fill = "none")
 
-ggsave("fig/boxplots-ts-120.png", height = 8.5, width = 8.5)
+ggsave("fig/boxplots-ts-120-appendix.png", height = 8.5, width = 8.5)
 
-p <- ggplot(data = tb |> 
+p <- ggplot(data = tb |>
               filter(penalty %in% c("10e-3", "10e-1", "10e+1", "10e+3"),
                      window == 120,
-                     date >= ymd("1940-01-01")),
+                     date >= ymd("1950-01-01")),
             aes(x = date, y = ts, color = penalty_f, group = penalty_f))
 
 p + geom_line() + 
@@ -71,7 +95,7 @@ ggsave("fig/ts-lineplot-120.png", height = 6.5, width = 8)
 p <- ggplot(data = tb |> 
               filter(penalty %in% c("10e-3", "10e-1", "10e+1", "10e+3"),
                      window == 120,
-                     date >= ymd("1940-01-01")),
+                     date >= ymd("1950-01-01")),
             aes(x = date, y = y_hat, color = penalty_f, group = penalty_f))
 
 p + geom_line() + 
